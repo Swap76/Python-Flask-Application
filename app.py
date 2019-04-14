a46e0bc36ec445a150226c91d6c422cf4887ecf5
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-04-14 10:38:44 +0530

    Heroku Deploy

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
